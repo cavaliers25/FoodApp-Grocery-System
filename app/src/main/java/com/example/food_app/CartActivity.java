2c158a7594ef8bd9e3bc3ae1641393915fb032ea
reverse-flash <jainanis947@gmail.com>
2021-04-22 18:41:08 +0530

    total price fix

--- app/src/main/java/com/example/food_app/CartActivity.java
+++ app/src/main/java/com/example/food_app/CartActivity.java
@@ -36,7 +36,7 @@ public class CartActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
     private Button NextProcessBtn;
     private TextView txtTotalAmount, txtMsg1;
-    private double overTotalPrice = 0;
+    private int overTotalPrice = 0;
 
 
 
@@ -96,13 +96,12 @@ public class CartActivity extends AppCompatActivity {
             protected void onBindViewHolder(@NonNull CartViewHolder holder, int position, @NonNull final Cart model)
             {
                 holder.txtProductQuantity.setText("Quantity = " + model.getQuantity());
-                holder.txtProductName.setText("Price " + model.getPname());
-                holder.txtProductPrice.setText(model.getPrice());
+                holder.txtProductPrice.setText("Price " +model.getPrice() + "₹");
                 holder.txtProductName.setText(model.getPname());
 
-                double oneTypeProductPrice = Double.parseDouble(model.getPrice().substring(model.getPrice().length()-1)) * Double.parseDouble(model.getQuantity());
+                int oneTypeProductPrice = ((Integer.valueOf(model.getPrice())))* Integer.valueOf(model.getQuantity());
                 overTotalPrice = overTotalPrice + oneTypeProductPrice;
-                txtTotalAmount.setText("Total Price = ₹ " + overTotalPrice);
+
 
                 holder.itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
